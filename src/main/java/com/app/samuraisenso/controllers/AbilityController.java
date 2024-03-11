3e4e69e04f9b61f391cb2a7b88f8aa27ffba79ae
Manuel Alvarez <mrmanuelalvarez@gmail.com>
2024-03-10 21:28:01 -0300

    some changes in controller

--- src/main/java/com/app/samuraisenso/controllers/AbilityController.java
+++ src/main/java/com/app/samuraisenso/controllers/AbilityController.java
@@ -2,32 +2,30 @@ package com.app.samuraisenso.controllers;
 
 import com.app.samuraisenso.domain.Ability;
 import com.app.samuraisenso.repositories.AbilityRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class AbilityController {
-    AbilityRepository repositorio;
-
-    public AbilityController(AbilityRepository repo){
-        this.repositorio = repo;
-    }
+    private AbilityRepository repositorio;
 
     @GetMapping("/api/createAbilities")
     public void createAbilities(){
-        Ability habilidad1 = new Ability(1L,"Hiten Mitsurugi", "Tecnica de la escuela de Kenshin Himura", "Battou");
-        Ability habilidad2 = new Ability(2L,"Kodachi Nito Ryuu", "Tecnica de la escuela de Aoshi Shinomori", "Kodachi");
-        Ability habilidad3 = new Ability(3L,"Gatotsu", "Tecnica de la escuela de Saito Hajime", "Kendo");
+        Ability ability1 = new Ability(1L,"Hiten Mitsurugi", "Tecnica de la escuela de Kenshin Himura", "Battou");
+        Ability ability2 = new Ability(2L,"Kodachi Nito Ryuu", "Tecnica de la escuela de Aoshi Shinomori", "Kodachi");
+        Ability ability3 = new Ability(3L,"Gatotsu", "Tecnica de la escuela de Saito Hajime", "Kendo");
 
-        repositorio.save(habilidad1);
-        repositorio.save(habilidad2);
-        repositorio.save(habilidad3);
+        repositorio.save(ability1);
+        repositorio.save(ability2);
+        repositorio.save(ability3);
     }
 
     @GetMapping("/api/abilities")
-    public List<Ability> getHabilidades (){
+    public List<Ability> getAbilities (){
         return repositorio.findAll();
     }
 }
